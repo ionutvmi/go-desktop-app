829a54068c31edb16dfa339324d72104c1bb2fc3
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-06-12 15:11:14 +0300

    Implements the sidebar and base layout for the application

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"go-desktop-app/sidebar"
 	"image/color"
 
 	"fyne.io/fyne/v2"
@@ -15,13 +17,57 @@ func main() {
 	w := a.NewWindow("Hello World")
 	w.Resize(fyne.NewSize(600, 400))
 
-	text1 := canvas.NewText("Hello", color.White)
-	text2 := canvas.NewText("There", color.White)
-	text3 := canvas.NewText("(right)", color.White)
-	content := container.New(layout.NewHBoxLayout(), text1, text2, layout.NewSpacer(), text3)
+	appTitle := canvas.NewText("Golang Utilities", color.White)
 
-	text4 := canvas.NewText("centered", color.White)
-	centered := container.New(layout.NewHBoxLayout(), layout.NewSpacer(), text4, layout.NewSpacer())
-	w.SetContent(container.New(layout.NewVBoxLayout(), content, centered))
+	sidePanel := sidebar.NewSidebar("Actions", []sidebar.Element{
+		{
+			Id:    "transform-text",
+			Label: "Transform text",
+		},
+		{
+			Id:    "transform-text-files",
+			Label: "Transform text files",
+		},
+		{
+			Id:    "transform-images",
+			Label: "Transform images",
+		},
+		{
+			Id:    "read-excel-file",
+			Label: "Read excel file",
+		},
+	})
+
+	mainPanel := container.NewWithoutLayout()
+
+	sidePanel.OnSelected = func(item sidebar.Element) {
+		fmt.Println("selected ", item.Id)
+		mainContent := canvas.NewText(
+			"Main content here"+item.Label,
+			color.White,
+		)
+
+		mainPanel.Objects = []fyne.CanvasObject{
+			container.New(layout.NewVBoxLayout(), mainContent),
+		}
+	}
+
+	var top fyne.CanvasObject = container.New(layout.NewCenterLayout(), appTitle)
+	var right fyne.CanvasObject = nil
+	var bottom fyne.CanvasObject = nil
+	var left fyne.CanvasObject = sidePanel.GetContent()
+
+	appContent := container.New(
+		layout.NewBorderLayout(top, bottom, left, right),
+		top, left,
+		mainPanel,
+	)
+
+	w.SetContent(appContent)
 	w.ShowAndRun()
 }
+
+// cta := widget.NewButton("click me", func() {
+// 	menuItems = append(menuItems, "Something new")
+// 	list.Refresh()
+// })
---
+++ sidebar/sidebar.go
@@ -0,0 +1,58 @@
+package sidebar
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+type Element struct {
+	Id    string
+	Label string
+}
+
+type Sidebar struct {
+	Title      string
+	Items      []Element
+	OnSelected func(item Element)
+}
+
+func NewSidebar(title string, items []Element) *Sidebar {
+	return &Sidebar{title, items, nil}
+}
+
+func (sidebar *Sidebar) GetContent() *fyne.Container {
+	sidebarTitle := container.New(layout.NewCenterLayout(), canvas.NewText(sidebar.Title, color.White))
+	list := sidebar.generateList()
+
+	return container.New(layout.NewBorderLayout(sidebarTitle, nil, nil, nil),
+		sidebarTitle,
+		list,
+	)
+}
+
+func (sidebar *Sidebar) generateList() *widget.List {
+	list := widget.NewList(
+		func() int {
+			return len(sidebar.Items)
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("template with some large text")
+		},
+		func(i widget.ListItemID, o fyne.CanvasObject) {
+			label := o.(*widget.Label)
+			label.SetText(sidebar.Items[i].Label)
+		})
+
+	list.OnSelected = func(id widget.ListItemID) {
+		if sidebar.OnSelected != nil {
+			sidebar.OnSelected(sidebar.Items[id])
+		}
+	}
+
+	return list
+}
