352b91dcbbdbdadf6b4ca33317c615be2c4ca8a4
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-06-18 18:23:46 +0300

    Updates the run commands to run on linux

--- run_app.ps1
+++ run_app.ps1
@@ -1,8 +1,18 @@
 
-# Used references
-# https://www.msys2.org/docs/ci/
-# https://www.msys2.org/wiki/Launchers/
-C:\tools\msys64\usr\bin\env.exe `
-    MSYSTEM=MINGW64 `
-    CHERE_INVOKING=1 `
-    /usr/bin/bash -lc "go run ."
+
+# Used on ubuntu
+if ($IsLinux) {
+    # First time run
+    # sudo apt-get install golang gcc libgl1-mesa-dev xorg-dev
+    go run .
+}
+
+if ($IsWindows) {
+    # Used references
+    # https://www.msys2.org/docs/ci/
+    # https://www.msys2.org/wiki/Launchers/
+    C:\tools\msys64\usr\bin\env.exe `
+        MSYSTEM=MINGW64 `
+        CHERE_INVOKING=1 `
+        /usr/bin/bash -lc "go run ."
+}
--- run_build_app.ps1
+++ run_build_app.ps1
@@ -1,8 +1,19 @@
 
-# Used references
-# https://www.msys2.org/docs/ci/
-# https://www.msys2.org/wiki/Launchers/
-C:\tools\msys64\usr\bin\env.exe `
-    MSYSTEM=MINGW64 `
-    CHERE_INVOKING=1 `
-    /usr/bin/bash -lc "go build -o dist/go-desktop-app.exe ."
+if ($IsLinux) {
+    # First time run
+    # sudo apt-get install golang gcc libgl1-mesa-dev xorg-dev
+    go build -o dist/go-desktop-app
+}
+
+
+if ($IsWindows) {
+
+    # Used references
+    # https://www.msys2.org/docs/ci/
+    # https://www.msys2.org/wiki/Launchers/
+    C:\tools\msys64\usr\bin\env.exe `
+        MSYSTEM=MINGW64 `
+        CHERE_INVOKING=1 `
+        /usr/bin/bash -lc "go build -o dist/go-desktop-app.exe ."
+
+}
